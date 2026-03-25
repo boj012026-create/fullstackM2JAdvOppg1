0648a2baac4cdf1c856dd327bca2afb154649f92
Jan Olav Berg <boj012026@gmail.com>
2026-03-25 14:50:07 +0100

    started a goemtric class for containing size and position, that handles collision etc

--- app.js
+++ app.js
@@ -1,29 +1,68 @@
-const world = document.getElementById("world");
-
-const player = {
-    node: document.getElementById("player"),
-    posX: 0,
-    posY: 0,
-    stepSize: 10,
-    unit: "px",
-    up: function() {
-	this.posY -= this.stepSize;
-	this.node.style.top = this.posY + this.unit; 
-    },
-    down: function() {
-	this.posY += this.stepSize;
-	this.node.style.top = this.posY + this.unit; 
-    },
-    left: function() {
-	this.posX -= this.stepSize;
-	this.node.style.left = this.posX + this.unit; 
-    },
-    right: function() {
-	this.posX += this.stepSize;
-	this.node.style.left = this.posX + this.unit; 
+/*Contains Geometric data: position and size*/
+class Geo {
+    constructur(x, y, width, height) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+    }
+    
+    inside(geo) {
     }
 }
 
+const world = {
+    limit: document.getElementById("world").getBoundingClientRect(),
+    contains: function(x, y) {
+	if (this.limit.left <= x && x <= this.limit.right &&
+	    this.limit.top <= y && y <= this.limit.bottom) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+};
+
+console.log(world);
+
+const player = {
+    node: document.getElementById("player"),
+    width: document.getElementById("player").offsetWidth,
+    height: document.getElementById("player").offsetHeight,
+    posX: world.limit.left,
+    posY: world.limit.top,
+    posT: 0,//temperary position
+    stepSize: 10,
+    unit: "px",
+    up: function() {
+	this.posT = this.posY - this.stepSize;
+	if(world.contains(this.posX, this.posT)) {
+	    this.posY = this.posT;
+	    this.node.style.top = this.posY + this.unit; 
+	}
+    },
+    down: function() {
+	this.posT = this.posY + this.stepSize;
+	if(world.contains(this.posX, this.posT)) {
+	    this.posY = this.posT;
+	    this.node.style.top = this.posY + this.unit; 
+	}
+    },
+    left: function() {
+	this.posT = this.posX - this.stepSize;
+	if (world.contains(this.posT, this.posY)) {
+	    this.posX = this.posT;
+	    this.node.style.left = this.posX + this.unit; 
+	}
+    },
+    right: function() {
+	this.posT = this.posX + this.stepSize;
+	if (world.contains(this.posT, this.posY)) {
+	    this.posX = this.posT;
+	    this.node.style.left = this.posX + this.unit; 
+	}
+    }
+}
 
 document.addEventListener("keydown", e => {
     console.log(e.key);
@@ -34,7 +73,3 @@ document.addEventListener("keydown", e => {
 	case "d": player.right(); break;
     }
 });
-
-function mDown(o) {
-    o.style.top = "20px";
-}
